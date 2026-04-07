ecf9b82446cbf4b348b7cbce866de81c79a28d59
Mikhail Voloshin <mvol@live.com>
2026-04-06 20:26:42 -0400

    Allegedly some CORS BS

--- yuppeeai-backend/src/localApi.ts
+++ yuppeeai-backend/src/localApi.ts
@@ -10,7 +10,7 @@ const context = {} as Context;
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 function collectBody(
