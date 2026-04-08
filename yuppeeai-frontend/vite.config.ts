1b1a639ad023a659d5fffd94acfe5841d4f9dc26
Mikhail Voloshin <mvol@live.com>
2026-04-08 18:47:39 -0400

    Giving more control to Stripe

--- yuppeeai-frontend/vite.config.ts
+++ yuppeeai-frontend/vite.config.ts
@@ -4,7 +4,16 @@ import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // Tell Vue not to warn about Stripe web components
+          isCustomElement: (tag) => tag.startsWith("stripe-"),
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
